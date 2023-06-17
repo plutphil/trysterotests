6e39c13f5ffee6cdb192fdbf24aeca2156e8e701
Your Name <you@example.com>
2023-06-17 22:10:10 +0200

    inital

---
+++ s.js
@@ -0,0 +1,9 @@
+import {joinRoom} from 'https://cdn.skypack.dev/trystero'
+
+const config = {appId: 'san_narciso_3d'}
+const room = joinRoom(config, 'yoyodyne')
+
+var dojoinroom = ()=>{
+    const inputroomid = document.getElementById("roomid");
+    const roomid = inputroomid.value;
+}
